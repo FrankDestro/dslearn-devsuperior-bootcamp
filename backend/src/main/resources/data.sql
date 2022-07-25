c077d2dc94de0741b97ebaa8f1e5ec637eab9d07
frank <frank-destro@outlook.com>
2022-07-25 18:37:02 -0300

    entitie Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -20,3 +20,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z',1); 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z',1); 
+
+INSERT INTO tb_resource (title, description, position, imgURI, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://www.oxfordwebstudio.com/user/pages/06.da-li-znate/sta-je-html/sta-je-html.jpg', 1, 1);
+INSERT INTO tb_resource (title, description, position, imgURI, type, offer_id) VALUES ('Forum', 'tire suas duvidas', 2, 'https://www.oxfordwebstudio.com/user/pages/06.da-li-znate/sta-je-html/sta-je-html.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, imgURI, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://www.oxfordwebstudio.com/user/pages/06.da-li-znate/sta-je-html/sta-je-html.jpg', 0, 1);
