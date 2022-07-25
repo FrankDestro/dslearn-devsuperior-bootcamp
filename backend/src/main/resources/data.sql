0f5a65083431898d23c813c7c19cafad1a71368a
frank <frank-destro@outlook.com>
2022-07-25 16:15:09 -0300

    entities User,Role and ORM created

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_user (name, email, password, created_At) VALUES ('Alex Brown', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', NOW());
+INSERT INTO tb_user (name, email, password, created_At) VALUES ('John White', 'john@gmail.com', '$2a$10$s2TkSddPKM8iJzcloCBk2uNafIyZsPlgp/FsbGpch1szuFQamxl02', NOW());
+INSERT INTO tb_user (name, email, password, created_At) VALUES ('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', NOW());
+
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
